0727bc44f43ff2128771e58de5c1a4f01112089a
Uni-tato <alex.lasenby@yahoo.co.nz>
2021-10-26 20:59:57 +1300

    Can now add players to a team using /jointeam command.
    This should not have taken nearly as long as it should.

--- src/com/unitato/milkwars/MilkWars.java
+++ src/com/unitato/milkwars/MilkWars.java
@@ -1,11 +1,44 @@
 package com.unitato.milkwars;
 
+import com.unitato.milkwars.commands.JoinTeam;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MilkWars extends JavaPlugin {
 
+    // Lists to store which player is in which team.
+    private static List<Player> cows = new ArrayList<Player>();
+    private static List<Player> sheep = new ArrayList<Player>();
+    public enum Teams { COW, SHEEP }
+
+    public static boolean addToTeam (Teams team, Player player) {
+        // Adds the player to team cow or sheep.
+        if (team == Teams.COW) {
+            return cows.add(player);
+        } else if (team == Teams.SHEEP) {
+            return sheep.add(player);
+        } else {
+            return false;
+        }
+    }
+    public static boolean inTeam(Teams team, Player player) {
+        // Checks if the player is in the provided team.
+        if (team == Teams.COW) {
+            return cows.contains(player);
+        } else if (team == Teams.SHEEP) {
+            return sheep.contains(player);
+        } else {
+            return false;
+        }
+    }
+
     @Override
     public void onEnable() {
+        getCommand("jointeam").setExecutor(new JoinTeam());
+
         getServer().getConsoleSender().sendMessage("§2[Milk-Wars] Plugin has been successfully enabled!");
     }
 
