8c3f98ff8c26bcf81506a47db1ca0dc7c6204d3b
Uni-tato <alex.lasenby@yahoo.co.nz>
2021-10-27 20:08:51 +1300

    Implemented clan leaders, and added getMember methods

--- src/com/hiuni/milkwars/Clan.java
+++ src/com/hiuni/milkwars/Clan.java
@@ -9,33 +9,84 @@ public class Clan {
 
     private String name;
     private List<Player> members; // A list of clan members;
+    private List<Player> leaders; // The leaders of the clan, should not have double ups with members.
     private int kills; // A counter for how many times clan members have killed other clam members.
     private int captures; // A counter for how many times the clan has successfully captured the enemy flag.
 
     Clan(String name) {
         this.name = name;
         this.members = new ArrayList<Player>();
+        this.leaders = new ArrayList<Player>();
         this.kills = 0;
         this.captures = 0;
     }
 
     public boolean addMember(Player player) {
         // Adds a player to the clan if they are not already a part of it.
-        if (this.members.contains(player)) {
+        if (this.hasMember(player)) {
             return false;
         } else {
             return this.members.add(player);
         }
     }
 
+    public boolean addLeader(Player player) {
+        // Makes player a leader of the clan.
+        if (this.leaders.contains(player)) {
+            return false;
+        } else if (this.members.contains(player)) {
+            this.members.remove(player); // If they're already a member then remove them from the member list.
+        }
+        return this.leaders.add(player); // and add them to the leaders list.
+    }
+
     public boolean removeMember(Player player) {
         // Removes a player from the clan if they are currently a member.
-        return this.members.remove(player);
+        if (this.members.contains(player)) {
+            return this.members.remove((player));
+        } else if (this.leaders.contains(player)) {
+            return this.leaders.remove(player);
+            // Should a leader be able to be removed, or should they have to be demoted to member first?
+        }
+        return false; // If we couldn't find the player to remove.
+    }
+
+    public boolean demoteLeader(Player player) {
+        // Demote a player from leader to member.
+        if (!this.leaders.contains(player)) {
+            return false;
+        }
+        this.leaders.remove(player);
+        this.members.add(player);
+        return true;
+    }
+
+    public List<Player> getMembers() {
+        // Returns a list of members (without leaders).
+        return members;
+    }
+
+    public List<Player> getLeaders() {
+        // Returns a list of clan leaders.
+        return leaders;
+    }
+
+    public List<Player> getAllMembers() {
+        // Returns a list of all the clan members (leaders and normal members).
+        List<Player> allMembers = new ArrayList<Player>();
+        allMembers.addAll(this.getMembers());
+        allMembers.addAll(this.getLeaders());
+        return allMembers;
     }
 
     public boolean hasMember(Player player) {
-        // Returns true if player is a member of this clan.
-        return this.members.contains(player);
+        // Returns true if player is a member (or leader) of this clan.
+        return (this.members.contains(player) || this.leaders.contains(player));
+    }
+
+    public boolean hasLeader(Player player) {
+        // Returns true if the player is a leader of the clan.
+        return this.leaders.contains(player);
     }
 
     public String getName() {
