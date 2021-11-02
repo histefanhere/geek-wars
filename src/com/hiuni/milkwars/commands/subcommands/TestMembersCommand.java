99cf474e7637eacbb933181fce9b34805a018c2f
Stefan <stefan.zdra@gmail.com>
2021-11-02 19:34:19 +1300

    list, promotion and demotion with CommandAPI

--- src/com/hiuni/milkwars/commands/subcommands/TestMembersCommand.java
+++ src/com/hiuni/milkwars/commands/subcommands/TestMembersCommand.java
@@ -1,15 +1,60 @@
 package com.hiuni.milkwars.commands.subcommands;
 
 import com.hiuni.milkwars.Clan;
+import com.hiuni.milkwars.ClanMember;
 import com.hiuni.milkwars.MilkWars;
 import dev.jorel.commandapi.CommandAPI;
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.EntitySelectorArgument;
 import dev.jorel.commandapi.arguments.MultiLiteralArgument;
 import dev.jorel.commandapi.arguments.PlayerArgument;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.text.Collator;
+import java.util.Collection;
+import java.util.TreeSet;
+
 public class TestMembersCommand {
+    private final CommandAPICommand membersList = new CommandAPICommand("list")
+            .withArguments(new MultiLiteralArgument("cows", "sheep"))
+            .executes((sender, args) -> {
+                int clanIndex = 0;
+                switch ((String) args[0]) {
+                    case "cows" -> clanIndex = 0;
+                    case "sheep" -> clanIndex = 1;
+                }
+                Clan clan = MilkWars.clans[clanIndex];
+
+                int clanSize = clan.getAllMembers().size();
+                if (clanSize == 0) {
+                    sender.sendMessage(String.format(ChatColor.YELLOW + "The %s is empty", clan.getName()));
+                    return;
+                }
+
+                // Tree sets are automatically sorted. Handy!
+                Collection<String> names = new TreeSet<>(Collator.getInstance());
+                String out = ChatColor.YELLOW + "Members of the " + clan.getName() + " (" +
+                        ChatColor.GOLD + "leaders" + ChatColor.YELLOW + "):\n";
+
+                for (ClanMember clanMember: clan.getAllMembers()) {
+                    if (clanMember.isLeader()) {
+                        names.add(ChatColor.GOLD + clanMember.getName());
+                    }
+                    else {
+                        names.add(ChatColor.YELLOW + clanMember.getName());
+                    }
+                }
+
+                String delimiter = ChatColor.YELLOW + ", ";
+                if (clanSize > 15) {
+                    delimiter = "\n";
+                }
+                out += String.join(delimiter, names);
+
+                sender.sendMessage(out);
+            });
+
     private final CommandAPICommand membersJoin = new CommandAPICommand("join")
             .withArguments(new PlayerArgument("player"))
             .withArguments(new MultiLiteralArgument("cows", "sheep"))
@@ -64,12 +109,55 @@ public class TestMembersCommand {
                 }
 
                 // If we've got here, the player isn't in any clan
-                CommandAPI.fail(ChatColor.RED + "You are not in any clan!");
+                CommandAPI.fail("Player isn't a part of a clan!");
+            });
+
+    private final CommandAPICommand membersPromote = new CommandAPICommand("promote")
+            .withArguments(new PlayerArgument("player"))
+            .executes((sender, args) -> {
+               Player player = (Player) args[0];
+
+                // Try and promote the player in both clans
+                for (Clan clan: MilkWars.clans) {
+                    if (clan.promote(player)) {
+                        sender.sendMessage(ChatColor.GREEN + "Promoted Successfully");
+                        player.sendMessage(
+                                String.format(ChatColor.GREEN + "You are now a leader of the %s!", clan.getName())
+                        );
+                        return;
+                    }
+                }
+
+                // If we've got here, the player isn't in any clan
+                CommandAPI.fail("Player must be a normal member of a clan to be promoted");
+            });
+
+    private final CommandAPICommand membersDemote = new CommandAPICommand("demote")
+            .withArguments(new EntitySelectorArgument("player", EntitySelectorArgument.EntitySelector.ONE_PLAYER))
+            .executes((sender, args) -> {
+                Player player = (Player) args[0];
+
+                // Try and demote the player in both clans
+                for (Clan clan: MilkWars.clans) {
+                    if (clan.demote(player)) {
+                        sender.sendMessage(ChatColor.GREEN + "Demoted Successfully");
+                        player.sendMessage(
+                                String.format(ChatColor.GREEN + "You have been demoted from the %s!", clan.getName())
+                        );
+                        return;
+                    }
+                }
+
+                // If we've got here, the player isn't a leader of a clan
+                CommandAPI.fail("Player must be a leader of a clan to be demoted");
             });
 
     public CommandAPICommand getCommand() {
         return new CommandAPICommand("members")
+                .withSubcommand(membersList)
                 .withSubcommand(membersJoin)
-                .withSubcommand(membersLeave);
+                .withSubcommand(membersLeave)
+                .withSubcommand(membersPromote)
+                .withSubcommand(membersDemote);
     }
 }
