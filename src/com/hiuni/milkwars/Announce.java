8ac04d8967becfde6ef60b76ac17eb4ff671024c
Uni-tato <alex.lasenby@yahoo.co.nz>
2021-11-02 14:56:30 +1300

    Created generic member and clan send methods

--- src/com/hiuni/milkwars/Announce.java
+++ src/com/hiuni/milkwars/Announce.java
@@ -1,25 +1,41 @@
 package com.hiuni.milkwars;
 
+import org.bukkit.ChatColor;
+
 public class Announce {
 
     // Send to clan.
-    public void sendToClan (Clan clan, String message) {
+    public void sendToClan (Clan clan, String message, ChatColor color) {
         // Sends a message to all signed in members of a clan.
-        sendToClan(clan, message, true);
+        sendToClan(clan, message, color, true);
     }
-    public void sendToClan (Clan clan, String message, boolean activeOnly) {
+    public void sendToClan (Clan clan, String message, ChatColor color, boolean activeOnly) {
         // Sends a message to clan members. If activeOnly = true then is sent to all
         // signed in members of the clan, otherwise is sent all members including
         // signed out members.
-        for (ClanMember member : clan.getAllMembers()) {
-            if (member.isSignedIn() || !(member.isSignedIn() || activeOnly)) {
-                member.sendMessage(message);
-            }
-        }
+        clanSend(clan, message, color, activeOnly, false);
     }
 
     // Send to clan leaders.
 
     // Send to all.
+
+    private void memberSend(ClanMember member, String message, ChatColor color) {
+        // Sends the message to the member formatted correctly.
+        member.sendMessage(color + "[Milk-wars] " + message);
+    }
+
+    private void clanSend(Clan clan, String message, ChatColor color,
+                          boolean activeOnly, boolean leaderOnly) {
+        for (ClanMember member : clan.getAllMembers()) {
+            if (
+                    (member.isSignedIn() || !(member.isSignedIn() || activeOnly)) &&
+                    (member.isLeader() || !(member.isLeader() || leaderOnly))
+            ) { // Could use clan.getLeaders, but I think this might be more efficient, no biggie either way lol.
+                memberSend(member, message, color);
+            }
+        }
+
+    }
 
 }
