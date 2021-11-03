1a7099e7e80c1e0efcc8caade6a13141aa480c58
Stefan <stefan.zdra@gmail.com>
2021-11-03 14:55:20 +1300

    Added all colours

--- src/com/hiuni/milkwars/commands/subcommands/SetChatColourCommand.java
+++ src/com/hiuni/milkwars/commands/subcommands/SetChatColourCommand.java
@@ -23,12 +23,29 @@ public class SetChatColourCommand {
     public HashMap<String, ChatColor> colours = new HashMap<>();
 
     public SetChatColourCommand() {
+        colours.put("dark_red", ChatColor.DARK_RED);
         colours.put("red", ChatColor.RED);
+        colours.put("gold", ChatColor.GOLD);
+        colours.put("yellow", ChatColor.YELLOW);
+        colours.put("dark_green", ChatColor.DARK_GREEN);
         colours.put("green", ChatColor.GREEN);
+        colours.put("aqua", ChatColor.AQUA);
+        colours.put("dark_aqua", ChatColor.DARK_AQUA);
+        colours.put("dark_blue", ChatColor.DARK_BLUE);
+        colours.put("blue", ChatColor.BLUE);
+        colours.put("light_purple", ChatColor.LIGHT_PURPLE);
+        colours.put("dark_purple", ChatColor.DARK_PURPLE);
+        // white here
+        colours.put("gray", ChatColor.GRAY);
+        colours.put("dark_gray", ChatColor.DARK_GRAY);
+        colours.put("black", ChatColor.BLACK);
 
     }
 
     public void updateNameTag(Player player) {
+        // Update the name tag of the player based on what team they're already in,
+        // and if they're signed in to a clan or not
+
         // Find the colour they're in
         ScoreboardManager manager = Bukkit.getScoreboardManager();
         assert manager != null;
