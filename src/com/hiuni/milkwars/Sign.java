c1d6fb96f5f12b506d1e60df00febff784a0f41a
Uni-tato <39069305+Uni-tato@users.noreply.github.com>
2021-11-16 18:14:12 +1300

    Signs can now use format codes
    This should not be used to make random fancy signs, this should only be used to make signs that are required to update to show clan stats. It is an unnecessary waste of resources (albeit not by much) if it's used just to make fancy signs.

--- src/com/hiuni/milkwars/Sign.java
+++ src/com/hiuni/milkwars/Sign.java
@@ -1,14 +1,14 @@
 package com.hiuni.milkwars;
 
-import org.apache.commons.lang.ObjectUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.data.BlockData;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Sign {
@@ -16,9 +16,9 @@ public class Sign {
     private Location location;
     private String rawString[];
 
-    private static List<Sign> existingSigns = new ArrayList<Sign>();
+    private static final List<Sign> existingSigns = new ArrayList<Sign>();
 
-    private static List<Material> signTypes = Arrays.asList(
+    private static final List<Material> signTypes = Arrays.asList(
             Material.ACACIA_WALL_SIGN,
             Material.WARPED_WALL_SIGN,
             Material.BIRCH_WALL_SIGN,
@@ -38,6 +38,32 @@ public class Sign {
             Material.SPRUCE_SIGN
     );
 
+    private static final HashMap<Character, String> FORMATREPLACEMENTS = new HashMap<>(){{
+        put('0', ChatColor.BLACK.toString());
+        put('1', ChatColor.DARK_BLUE.toString());
+        put('2', ChatColor.DARK_GREEN.toString());
+        put('3', ChatColor.DARK_AQUA.toString());
+        put('4', ChatColor.DARK_RED.toString());
+        put('5', ChatColor.DARK_PURPLE.toString());
+        put('6', ChatColor.GOLD.toString());
+        put('7', ChatColor.GRAY.toString());
+        put('8', ChatColor.DARK_GRAY.toString());
+        put('9', ChatColor.BLUE.toString());
+        put('a', ChatColor.GREEN.toString());
+        put('b', ChatColor.AQUA.toString());
+        put('c', ChatColor.RED.toString());
+        put('d', ChatColor.LIGHT_PURPLE.toString());
+        put('e', ChatColor.YELLOW.toString());
+        put('f', ChatColor.WHITE.toString());
+
+        put('k', ChatColor.MAGIC.toString());
+        put('l', ChatColor.BOLD.toString());
+        put('m', ChatColor.STRIKETHROUGH.toString());
+        put('n', ChatColor.UNDERLINE.toString());
+        put('o', ChatColor.ITALIC.toString());
+        put('r', ChatColor.RESET.toString());
+    }};
+
     public Sign(Location location, String[] lines) {
         this.location = location;
         this.rawString = lines;
@@ -52,7 +78,7 @@ public class Sign {
 
     public void update(){
         Block block = getBlock();
-        if (!signTypes.contains(block.getType())) {
+        if (!signTypes.contains(block.getType())) { // TODO could maybe use (state instanceof Sign) instead.
             // The sign no longer exists.
             Bukkit.getConsoleSender().sendMessage("This block is no longer a sign.");
             existingSigns.remove(this);
@@ -62,8 +88,7 @@ public class Sign {
         org.bukkit.block.Sign s = getSign();
         s.setEditable(true);
         for (int i = 0; i < 4; i++) {
-            s.setLine(i, format(rawString[i]));
-            Bukkit.getConsoleSender().sendMessage(s.getLine(i));
+            s.setLine(i, formatLine(rawString[i]));
         }
 
         //s.update();
@@ -74,8 +99,36 @@ public class Sign {
         Bukkit.getScheduler().runTask(MilkWars.getInstance(), (Runnable) s::update);
     }
 
-    private static String format(String text) {
-        return text.replace("#MW", "this is a test");
+    private static String formatLine(String text) {
+        StringBuilder sb = new StringBuilder();
+        char[] charArray = text.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+
+            // For format codes.
+            if (charArray[i] == '&') {
+                try {
+                    String formatStr = FORMATREPLACEMENTS.get(charArray[i + 1]);
+                    if (formatStr == null) {
+                        sb.append('&');
+                    }
+                    sb.append(formatStr);
+                    i++;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    sb.append('&');
+                }
+            }
+
+            // For variable codes.
+            if (charArray[i] == '#') {
+                
+            }
+
+            else {
+                sb.append(charArray[i]);
+            }
+        }
+        Bukkit.getConsoleSender().sendMessage(sb.toString());
+        return sb.toString();
     }
 
     public Location getLocation() {
